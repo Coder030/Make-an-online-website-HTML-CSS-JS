c5c5c2d761742de02cc9a0852f79f715e99271d4
Coder030 <122353053+Coder030@users.noreply.github.com>
2023-12-23 22:12:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function Join(){
+    window.location.href = "JoinClub.html";
+}
+function News(){
+    window.location.href = "News.html";
+}
+function Blog(){
+    window.location.href = "Blogs.html";
+}
+
+
